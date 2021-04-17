704d3e3b846a4a3e21d174b2284bb26860ec90d4
bhavanimahalingam <58926863+bhavanimahalingam@users.noreply.github.com>
2021-04-17 16:13:41 +0530

    Delete src directory

--- src/test/TestSquare.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-
-public class SquareJunitTesting{
-   public void test(){
-	   main test=new main();
-	   int output=test.square(4);
-	   AssertEquals(16,output);
-   }
-}
--- src/test/countAtest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-
-public class CountJunitTesting{
-   public void test(){
-	   main test=new main();
-	   int output=test.countA("bavani");
-	   AssertEquals(2,output);
-   }
-}
