797cfc4a9084226e925fcd198fe67fc660f40688
bhavanimahalingam <58926863+bhavanimahalingam@users.noreply.github.com>
2021-04-17 17:18:56 +0530

    Delete newinstance.tf

--- newinstance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "MyfirstInstance" {
-   count = 1
-   ami = "ami-0b55b3040fb5668a3"
-   instance_type = "t2.micro" 
-   tags = {
-     "Name" = "demoec2test-${count.index}"
-   }
-}
